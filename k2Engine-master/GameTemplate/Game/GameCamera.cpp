e95249297fc9779dcaa64de93aa995bfbec92312
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-03-11 16:33:06 +0900

    ゲームカメラの追加

--- k2Engine-master/GameTemplate/Game/GameCamera.cpp
+++ k2Engine-master/GameTemplate/Game/GameCamera.cpp
@@ -1,2 +1,33 @@
 #include "stdafx.h"
 #include "GameCamera.h"
+
+
+GameCamera::GameCamera()
+{
+
+}
+
+GameCamera::~GameCamera()
+{
+	DeleteGO(this);
+}
+
+bool GameCamera::Start()
+{
+	//注視点から視点までのベクトルを設定。
+	m_toCameraPos.Set(1.0f, 125.0f, -60.0f);
+	//カメラの座標を取得。
+	//プレイヤーのインスタンスを探す。
+	m_player = FindGO<Player>("player");
+
+	//カメラのニアクリップとファークリップを
+	g_camera3D->SetNear(1.0f);
+	g_camera3D->SetFar(2000.0f);
+
+	return true;
+}
+
+void GameCamera::Update()
+{
+
+}
--- k2Engine-master/GameTemplate/Game/GameCamera.h
+++ k2Engine-master/GameTemplate/Game/GameCamera.h
@@ -1,9 +1,19 @@
 #pragma once
 
+class Player;
 
 class GameCamera:public IGameObject
 {
 public:
+	GameCamera();
+	~GameCamera();
+	bool Start();
+	void Update();
+	
 
+	//メンバ変数
+	GameCamera* m_gameCamera;//カメラ。
+	Player* m_player;//プレイヤー。
+	Vector3 m_toCameraPos;//注視点から視点に向かうベクトル。
 };
 
