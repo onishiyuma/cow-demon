2ae0a9e43f71196489e5457953e2ee0559bb7eae
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-03-13 10:29:08 +0900

    カメラの変更

--- k2Engine-master/GameTemplate/Game/GameCamera.cpp
+++ k2Engine-master/GameTemplate/Game/GameCamera.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "GameCamera.h"
+#include "Player.h"
 
 
 GameCamera::GameCamera()
@@ -15,19 +16,62 @@ GameCamera::~GameCamera()
 bool GameCamera::Start()
 {
 	//注視点から視点までのベクトルを設定。
-	m_toCameraPos.Set(1.0f, 125.0f, -60.0f);
-	//カメラの座標を取得。
+	m_toCameraPos.Set(1.0f, 125.0f, -250.0f);
+	//カメラ座標を取得。
 	//プレイヤーのインスタンスを探す。
 	m_player = FindGO<Player>("player");
 
 	//カメラのニアクリップとファークリップを
 	g_camera3D->SetNear(1.0f);
-	g_camera3D->SetFar(2000.0f);
+	g_camera3D->SetFar(10000.0f);
 
 	return true;
 }
 
 void GameCamera::Update()
 {
+	//注視点を計算する。
+	Vector3 target = m_player->m_position;
+	//プレイヤーの注視点。
+	target.y += 115.0f;
 
+	Vector3 toCameraPosOld = m_toCameraPos;
+	//パッドの入力を使ってカメラを回す。
+	float x = g_pad[0]->GetRStickXF();
+	float y = g_pad[0]->GetRStickYF();
+	//Y軸周りの回転
+	Quaternion qRot;
+	qRot.SetRotationDeg(Vector3::AxisY, 3.0f * x);
+	qRot.Apply(m_toCameraPos);
+	//X軸周りの回転
+	Vector3 axisX;
+	axisX.Cross(Vector3::AxisY, m_toCameraPos);
+	axisX.Normalize();
+	qRot.SetRotationDeg(axisX, 1.45f * y);
+	qRot.Apply(m_toCameraPos);
+	//カメラの回転の上限をチェックする。
+	//注視点から視点までのベクトルを正規化する。
+	//正規化すると、ベクトルの大きさが１になる。
+	//大きさが１になるということは、ベクトルから強さがなくなり、方向のみの情報となるということ。
+	
+	//カメラの移動制御
+	Vector3 toPosDir = m_toCameraPos;
+	toPosDir.Normalize();
+	if (toPosDir.y < -0.1f)
+	{
+		m_toCameraPos = toCameraPosOld;
+	}
+	if (toPosDir.y > 0.1f)
+	{
+		m_toCameraPos = toCameraPosOld;
+	}
+
+	//視点を計算する。
+	Vector3 pos = target + m_toCameraPos;
+	//メインカメラに注視点と視点を設定する。
+	g_camera3D->SetTarget(target);
+	g_camera3D->SetPosition(pos);
+
+	//カメラの更新。
+	g_camera3D->Update();
 }
