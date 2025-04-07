0367594028ecc2bba77def66219ff958bdd3be3e
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-04-07 12:31:46 +0900

    クロスヘアーの追加

---
+++ k2Engine-master/GameTemplate/Game/CrossHair.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "CrossHair.h"
+
+bool CrossHair::Start()
+{
+	//画像を読み込む。
+	m_spriteRender.Init("Assets/sprite/CrossHair.DDS", 120.0f, 120.0f);
+	//座標をセット。
+	m_spriteRender.SetPosition(m_position);
+
+	return true;
+}
+
+CrossHair::CrossHair()
+{
+
+}
+
+CrossHair::~CrossHair()
+{
+	DeleteGO(this);
+}
+
+void CrossHair::Update()
+{
+
+}
+
+void CrossHair::Render(RenderContext& rc)
+{
+	m_spriteRender.Draw(rc);
+}
