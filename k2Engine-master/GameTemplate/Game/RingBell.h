a566d34b9bb270f7ca2a710167ea8ee2a861d838
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-04-16 09:45:29 +0900

    RIngBellの変更。

--- k2Engine-master/GameTemplate/Game/RingBell.h
+++ k2Engine-master/GameTemplate/Game/RingBell.h
@@ -9,7 +9,13 @@ public:
 	bool Start();
 	void Update();
 	void Render(RenderContext& rc);
+	//コリジョンの作成。
+	void CreateCollision();
 
 	//メンバ変数。
-	Vector3 m_position;
+	PhysicsStaticObject m_physicsStaticObject;//判定。
+	CollisionObject*m_collisionObject;//コリジョンオブジェクト。
+	ModelRender m_modelRender;//モデルレンダー。
+	Vector3 m_position = Vector3{ 70.0f, 90.0f, -1345.0f };//座標
+	Vector3 m_collisionScale = Vector3{ 240.0f,50.0f,200.0f };//コリジョンの大きさ。
 };
