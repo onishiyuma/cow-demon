9fa1eb9de030c5c0f2877db1670aa4ec0fb851fa
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-04-22 16:22:39 +0900

    UIの変更

--- k2Engine-master/GameTemplate/Game/UIheal.cpp
+++ k2Engine-master/GameTemplate/Game/UIheal.cpp
@@ -6,11 +6,11 @@
 namespace
 {
 	//‰ñ•œ‰ñ”1
-	Vector3 SOUL_POSITION = Vector3(-850.0f, -450.0f, 0.0f);
+	Vector3 SOUL_POSITION = Vector3(-600.0f, -450.0f, 0.0f);
 	//‰ñ•œ‰ñ”2
 	Vector3 SOUL_POSITION2 = Vector3(-725.0f, -450.0f, 0.0f);
-	//‰ñ•œ‰ñ”2
-	Vector3 SOUL_POSITION3 = Vector3(-600.0f, -450.0f, 0.0f);
+	//‰ñ•œ‰ñ”3
+	Vector3 SOUL_POSITION3 = Vector3(-850.0f, -450.0f, 0.0f);
 }
 
 
@@ -37,21 +37,39 @@ m_player = FindGO<Player>("player");
 	m_soulSprite3.Init("Assets/UI/gage.DDS", 150, 150);
 	m_soulSprite3.SetPosition(SOUL_POSITION3);
 
+	m_useHeal = 3;
+
 	return true;
 }
 
 void UIheal::Update()
 {
-	m_soulSprite3.Update();
-	m_soulSprite2.Update();
 	m_soulSprite1.Update();
-
-
+	m_soulSprite2.Update();
+	m_soulSprite3.Update();
 }
 
 void UIheal::Render(RenderContext& rc)
 {
-	m_soulSprite1.Draw(rc);
-	m_soulSprite2.Draw(rc);
-	m_soulSprite3.Draw(rc);
+
+	if (m_useHeal >= 3)
+	{
+		m_soulSprite1.Draw(rc);
+		m_soulSprite2.Draw(rc);
+		m_soulSprite3.Draw(rc);
+	}
+	if (m_useHeal ==2)
+	{
+		m_soulSprite2.Draw(rc);
+		m_soulSprite3.Draw(rc);
+	}
+	else if (m_useHeal == 1)
+	{
+		m_soulSprite3.Draw(rc);
+	}
+	else if (m_useHeal <= 0)
+	{
+		m_deleteFlag = true;
+		DeleteGO(this);
+	}
 }
