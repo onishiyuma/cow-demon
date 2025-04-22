cb2d4235a5f99de7e4d8f219afd921dd412922bd
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-04-22 16:21:58 +0900

    GameCameraの変更

--- k2Engine-master/GameTemplate/Game/GameCamera.h
+++ k2Engine-master/GameTemplate/Game/GameCamera.h
@@ -15,18 +15,12 @@ public:
 		m_isLocked = isLock;
 	}
 
-	//メンバ変数
-	GameCamera* m_gameCamera;//カメラ。
+	//メンバ変数。
 	Player* m_player;//プレイヤー。
 	Vector3 m_toCameraPos;//注視点から視点に向かうベクトル。
 	int m_nearClip = 1.0f;//ニアークリップ
 	int m_furClip = 20000.0f;//ファークリップ
-	const int m_half=2;//半分にする
-
-
-	int m_nearClip = 1.0f;//ニアークリップ。
-	int m_furClip = 20000.0f;//ファークリップ。
-	const int m_half=2;//半分にする。
+	const float m_half=2;//半分にする
 	bool m_isLocked = false;//カメラを停止するようフラグ。
 };
 
