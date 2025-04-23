6d9149532e3f0189cf660a12446fa02ccc0a0bbb
sakaifuga0807 <tamamokakegohan@icloud.com>
2025-04-23 14:54:16 +0900

    Enemyの変更。

--- k2Engine-master/GameTemplate/Game/Enemy.h
+++ k2Engine-master/GameTemplate/Game/Enemy.h
@@ -101,6 +101,7 @@ private:
 	/*Tou* m_tou = nullptr;*/
 	ModelRender m_modelRender;
 	Vector3 m_position;
+	Vector3 m_farstPosition=Vector3::Zero;
 	Vector3 m_forward = Vector3::AxisZ;
 	Vector3 m_moveSpeed;
 	const Vector3 m_stopMove = Vector3::Zero;
