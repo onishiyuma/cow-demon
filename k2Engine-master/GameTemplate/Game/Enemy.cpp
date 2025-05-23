b05038e00eb5297dc24095c2271ae15cba328556
OnishiYuma <167502130+onishiyuma@users.noreply.github.com>
2025-05-23 18:49:18 +0900

    空の明るさ変更 (#66)

--- k2Engine-master/GameTemplate/Game/Enemy.cpp
+++ k2Engine-master/GameTemplate/Game/Enemy.cpp
@@ -61,6 +61,7 @@ bool Enemy::Start()
 	m_game = FindGO<Game>("game");
 	m_gameCamera = FindGO<GameCamera>("gamecamera");
 	
+	
 	//乱数を初期化する。
 	srand((unsigned)time(NULL));
 	m_forward = Vector3::AxisZ;
@@ -343,6 +344,7 @@ void Enemy::Collision()
 		{
 			m_gameCamera->m_isGameOver = true;
 			DeleteGO(this);
+
 			break;
 		}
 	}
@@ -516,7 +518,7 @@ void Enemy::ProcessCommonStateTransition()
 			//ベクトルを正規化する。
 			diff.Normalize();
 			//移動速度を設定する。
-			m_moveSpeed = diff * 250.0f;
+			m_moveSpeed = diff * 100.0f;
 			//攻撃できる距離なら。
 			int ram = rand() % 100;
 
